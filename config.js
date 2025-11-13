168704e3b899ab33a7a428beb2959d3ad1e84864
nn <205666415+guessmesome@users.noreply.github.com>
2025-11-13 19:38:35 +0700

    Update config.js

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ const CONFIG = {
     products: {
         1: 'https://vldmnt.ru/go/scf0dwnsuf',
         2: 'https://vldmnt.ru/go/s68og7vdwk',
-        3: 'https://vldmnt.ru/go/sb0p3irl2n',
-        4: 'https://vldmnt.ru/go/su61r5myi7'
+        3: 'https://vldmnt.ru/go/su61r5myi7',
+        4: 'https://vldmnt.ru/go/sb0p3irl2n'
     }
 };

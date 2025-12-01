62e395decc700e804586a154ffc8952b34edbde9
nn <205666415+guessmesome@users.noreply.github.com>
2025-12-01 21:24:33 +0700

    Update config.js

--- config.js
+++ config.js
@@ -12,28 +12,28 @@ const config = {
             title: "Дебетовая карта",
             subtitle: "Кэшбэк до 3000₽<br>в месяц",
             image: "./images/Product_1.png",
-            url: "https://trnsfx.ru/go/s3wq40bpow"
+            url: "https://vldmnt.ru/go/scf0dwnsuf"
         },
         {
             id: 2,
             title: "Кредитная карта",
             subtitle: "Снятие наличных<br>без комиссии",
             image: "./images/Product_2.png",
-            url: "https://trnsfx.ru/go/s0uusze41d"
+            url: "https://vldmnt.ru/go/s68og7vdwk"
         },
         {
             id: 3,
             title: "Кредит наличными",
             subtitle: "До 90 дней без<br>первого платежа",
             image: "./images/Product_3.png",
-            url: "https://trnsfx.ru/go/s2s0j4ozv5"
+            url: "https://vldmnt.ru/go/sb0p3irl2n"
         },
         {
             id: 4,
             title: "Займ на карту",
             subtitle: "Под выгодный<br>процент",
             image: "./images/Product_4.png",
-            url: "https://trnsfx.ru/go/sh5ztmjczt"
+            url: "https://vldmnt.ru/go/su61r5myi7"
         }
     ]
 };
